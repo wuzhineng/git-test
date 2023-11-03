04aff7d07781eb1a33b7ed7671086ddbcfbfe943
wuzhineng <2283178140@qq.com>
2023-11-03 14:35:57 +0800

    git testing

---
+++ src/main/java/com/atguigu/GitTest4.java
@@ -0,0 +1,9 @@
+package com.atguigu;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class GitTest4 {
+    public static void main(String[] args) {
+        System.out.println("git testing");
+    }
+}
