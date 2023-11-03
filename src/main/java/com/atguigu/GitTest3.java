8ee738b530ef06954b800d764457d62b27fac54b
wuzhineng <83151710+wuzhineng@users.noreply.github.com>
2023-11-03 15:26:45 +0800

    Update GitTest3.java

--- src/main/java/com/atguigu/GitTest3.java
+++ src/main/java/com/atguigu/GitTest3.java
@@ -2,6 +2,6 @@ package com.atguigu;
 
 public class GitTest3 {
     public static void main(String[] args) {
-        System.out.println("GitTest33333333334444455555");
+        System.out.println("GitTest333333333344444555556666");
     }
 }
