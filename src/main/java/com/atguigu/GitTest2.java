d83474f743393e6e4b0af9e6b94688f37120dc5c
wuzhineng <83151710+wuzhineng@users.noreply.github.com>
2023-11-03 11:09:24 +0800

    commit1

--- src/main/java/com/atguigu/GitTest2.java
+++ src/main/java/com/atguigu/GitTest2.java
@@ -2,6 +2,6 @@ package com.atguigu;
 
 public class GitTest2 {
     public static void main(String[] args) {
-        System.out.println("GitTest222222");
+        System.out.println("GitTest2222223333");
     }
 }
