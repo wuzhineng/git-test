faa73b254bba1bfb7f2dd23cff919b0ca6d3762b
wuzhineng <2283178140@qq.com>
2023-11-03 13:29:51 +0800

    hot-fix test7

--- src/main/java/com/atguigu/Test4.java
+++ src/main/java/com/atguigu/Test4.java
@@ -2,6 +2,6 @@ package com.atguigu;
 
 public class Test4 {
     public static void main(String[] args) {
-        System.out.println("346542");
+        System.out.println("3465424654756");
     }
 }
